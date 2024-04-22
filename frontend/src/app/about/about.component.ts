e3f86eb476683519ec1be7e6f4630f0ad8ec9b8c
shynottea <kulekesh.m1705@mail.ru>
2024-04-23 00:12:47 +0500

    about page

--- frontend/src/app/about/about.component.ts
+++ frontend/src/app/about/about.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { Header2Component } from '../header2/header2.component';
+import { FooterComponent } from '../footer/footer.component';
+import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [Header2Component],
+  imports: [Header2Component, FooterComponent,
+    RouterModule, 
+    RouterOutlet,
+  ],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
