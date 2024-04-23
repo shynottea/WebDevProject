489a6f7eb164b8fcdb8b3b4a2241289b65bbf4c6
shynottea <kulekesh.m1705@mail.ru>
2024-04-23 15:21:18 +0500

    added login

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
+import { Header2Component } from '../header2/header2.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
     RouterModule, 
-    RouterOutlet
+    RouterOutlet,
+    Header2Component,
+    FooterComponent
   ],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
