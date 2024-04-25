eca1c1ad0d263330e96f30875f515c552519fe1b
shynottea <kulekesh.m1705@mail.ru>
2024-04-25 07:38:03 +0500

    design changes

--- frontend/src/app/highlights/highlights.component.ts
+++ frontend/src/app/highlights/highlights.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from '../footer/footer.component';
-import { RouterModule, RouterOutlet } from '@angular/router';
+import { ActivatedRoute, RouterModule, RouterOutlet } from '@angular/router';
 import { Header2Component } from '../header2/header2.component';
+import { BackendService } from '../backend.service';
+
 
 @Component({
   selector: 'app-highlights',
@@ -13,5 +15,25 @@ import { Header2Component } from '../header2/header2.component';
   styleUrl: './highlights.component.css'
 })
 export class HighlightsComponent {
-
+   // @ts-ignore
+   products: Product[] = [];
+   // @ts-ignore
+   name: string;
+   constructor(private route: ActivatedRoute,
+               private backendservice: BackendService) {
+   }
+ 
+   ngOnInit(): void {
+     this.getCategoryProducts();
+   }
+ 
+   getCategoryProducts(): void {
+     this.route.paramMap.subscribe((params) => {
+       // @ts-ignore
+       const id = +params.get('id');
+       this.backendservice.getCategoryProducts(id).subscribe((data) => {
+         this.products = data;
+       });
+     });
+   }
 }
